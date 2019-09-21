ebcc693e4359a18c42062c9e12f76e5abf38fab0
BrianLeabold <livingsingl@hotmail.com>
2019-09-20 22:39:33 -0700

    Added dayjs - format date/time

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,6 +5,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 
 const styles = {
@@ -24,13 +26,14 @@ const styles = {
 
 class Post extends Component {
     render() {
+        dayjs.extend(relativeTime);
         const { classes, post: { body, userName, userImage, createdAt, commentCount, likeCount } } = this.props;
         return (
             <Card className={classes.card}>
                 <CardMedia image={userImage} className={classes.image} title="Pofile Image" />
                 <CardContent className={classes.content}>
                     <Typography variant="h5" component={Link} to={`/users/${userName}`}>{userName}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+                    <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                     <Typography variant="body1">{body}</Typography>
                     <Typography variant="caption">Comments - {commentCount}</Typography>
                     <br />
