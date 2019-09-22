7e62be4407719a41322220fb1c9f453c52c3c527
BrianLeabold <livingsingl@hotmail.com>
2019-09-21 23:04:20 -0700

    Added AuthRoute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 import './App.css';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
@@ -7,13 +8,25 @@ import themeFile from './util/theme';
 
 //Components
 import NavBar from './components/NavBar';
+import AuthRoute from './util/AuthRoute';
 //Pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
 const theme = createMuiTheme(themeFile);
+let authenticated;
 const token = localStorage.cbIdToken;
+if (token) {
+
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login';
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 
 class App extends Component {
@@ -27,8 +40,8 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home}></Route>
-                <Route exact path="/login" component={login}></Route>
-                <Route exact path="/signup" component={signup}></Route>
+                <AuthRoute exact path="/login" component={login} authenticated={authenticated}></AuthRoute>
+                <AuthRoute exact path="/signup" component={signup} authenticated={authenticated}></AuthRoute>
               </Switch>
             </div>
           </Router>
